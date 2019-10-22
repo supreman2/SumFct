477d9a12fa3a61d57a012425cbaf42a6abeb9e57
supreman <supreman2@gmail.com>
2019-10-23 00:40:42 +0300

    https://www.codewars.com/kata/559a28007caad2ac4e000083/train/java

--- src/SumFct.java
+++ src/SumFct.java
@@ -2,26 +2,25 @@ import java.math.BigInteger;
 public class SumFct {
     public static BigInteger perimeter(BigInteger n) {
 
-        int cur1 = 0;
-        int cur2 = 0;
-        int res = 0;
-        int delta;// = 0;
+        BigInteger cur1 = BigInteger.valueOf(0);
+        BigInteger cur2 = BigInteger.valueOf(0);
+        BigInteger res = BigInteger.valueOf(0);
+        BigInteger delta;// = 0;
 
         for(int i=1;i<=n.intValue()+1;i++) {
-
             if (i==1){
-                delta = 1;
-                res += delta;
-                cur1 = 0;
+                delta = BigInteger.valueOf(1);
+                res = res.add(delta);
+                cur1 = BigInteger.valueOf(0);
                 cur2 = delta;
                 continue;
             }
 
-            delta = cur1 + cur2;
-            res += delta;
+            delta = cur1.add(cur2);
+            res = res.add(delta);
             cur1 = cur2;
             cur2 = delta;
         }
-        return BigInteger.valueOf(res*4);
+        return res.multiply(BigInteger.valueOf(4));
     }
 }
