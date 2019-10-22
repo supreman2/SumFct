c0da0269d210591dc92742894809a1aa8d94c913
supreman <supreman2@gmail.com>
2019-10-23 00:10:45 +0300

    https://www.codewars.com/kata/559a28007caad2ac4e000083/train/java

---
+++ src/SumFct.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+public class SumFct {
+    public static BigInteger perimeter(BigInteger n) {
+
+        int cur1 = 0;
+        int cur2 = 0;
+        int res = 0;
+        int delta = 0;
+
+        for(int i=1;i<=n.intValue()+1;i++) {
+
+            if (i==1){
+                delta = 1;
+                res += delta;
+                cur1 = 0;
+                cur2 = delta;
+                continue;
+            }
+
+            delta = cur1 + cur2;
+            res += delta;
+            cur1 = cur2;
+            cur2 = delta;
+        }
+        return BigInteger.valueOf(res*4);
+    }
+}
